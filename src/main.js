44a80b3fd42b8924cc425460141c6d139ff89cc5
Herisson Nogueira <herisson.nogueira002@gmail.com>
2023-09-02 00:28:28 -0300

    construindo a faq e o rodapé

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -9,16 +9,28 @@ document.addEventListener('DOMContentLoaded', function() {
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is-active')
+            botao.target.classList.add('shows__tabs__button--is-active');
         })
     } 
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+    
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active')
+        buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
@@ -26,6 +38,6 @@ function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active')
+        tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
